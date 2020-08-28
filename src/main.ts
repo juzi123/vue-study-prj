24788572baf9541346891b2ceb49446ad0230a61
bjj <505956694@qq.com>
2020-08-28 11:03:04 +0800

    侧边栏demo

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import Vue from "vue";
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
-
+Vue.use(Antd);
+// 4. 创建和挂载根实例。
+// 记得要通过 router 配置参数注入路由，
+// 从而让整个应用都有路由功能
 new Vue({
   router,
   store,
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,8 +2,14 @@ import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Home from "../views/Home.vue";
 
+// 使用模块化机制编程，导入Vue和VueRouter，要调用 Vue.use(VueRouter)
 Vue.use(VueRouter);
 
+// 2. 定义路由
+// 每个路由应该映射一个组件。 其中"component" 可以是
+// 通过 Vue.extend() 创建的组件构造器，
+// 或者，只是一个组件配置对象。
+// 我们晚点再讨论嵌套路由。
 const routes: Array<RouteConfig> = [
   {
     path: "/",
@@ -21,6 +27,7 @@ const routes: Array<RouteConfig> = [
   }
 ];
 
+// 3. 创建 router 实例，然后传 `routes` 配置
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
